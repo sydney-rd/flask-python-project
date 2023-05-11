838e2bee17f528297d5a5a08acdb6b096c06b6ef
sydney-rd <sydneydavid56@gmail.com>
2023-05-11 15:33:52 -0400

    adding candy data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask, request, jsonify
+from flask import Flask, request, jsonify
 from peewee import * 
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
@@ -20,3 +20,16 @@ class Candy(BaseModel):
 db.connect()
 db.drop_tables([Candy])
 db.create_tables([Candy])
+
+Candy(name="Licorice", flavor="Strawberry", shape="bites").save()
+Candy(name="Sour Straws", flavor="Pink Lemonade", shape="long").save()
+Candy(name="Lollipop", flavor="Rasberry", shape="sphere").save()
+Candy(name="Twix", flavor="chocolate", shape="bites").save()
+Candy(name="Jolly Rancher", flavor="Blue Rasperry", shape="tiny babies").save()
+Candy(name="Chocolate bar", flavor="dark chocolate", shape="bar").save()
+
+app = Flask(__name__)
+
+@app.route("/candy/", methods=["GET", "POST"])
+@app.route("/candy/<id>", methods=["GET", "PUT", "DELETE"])
+def endpoint(id=None):
